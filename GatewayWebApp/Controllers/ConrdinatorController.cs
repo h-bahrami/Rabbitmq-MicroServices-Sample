5751955d034a7dc28b474a3816766a1309c39180
bahrami <h.bahrami@live.com>
2019-08-19 16:59:14 +0200

    Uploading the solution

---
+++ GatewayWebApp/Controllers/ConrdinatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayWebApp.Controllers
+{
+    public class ConrdinatorController : Controller
+    {
+        [Route("Cordinator/Index")]
+        public IActionResult Index()
+        {
+            ViewBag.Message = $"Process Started {DateTime.Now}";
+            return View();
+        }
+
+        //[Route("Cordinator/StartProcess")]
+        //public IActionResult StartProcess()
+        //{            
+        //    return View();
+        //}
+    }
+}
